1bf805eeb8e1d5e1e8a9f5d7b5e45bd85fead9b6
Terence Gould <terence@thegoulds.me>
2018-11-20 14:57:05 +0000

    Initial Creation

--- dbp2WD/dbp2WD.py
+++ dbp2WD/dbp2WD.py
@@ -1,1 +1,16 @@
-print ("Hello")
+import urllib, json
+def main():
+    print ("Hello")
+    getWD("Test")
+
+def getWD(dbp_id):
+    #Function to call the Wikipedia API
+    #using the dbPedia ID as reference
+    #Will create response in JSON
+    # which can be used to get the Q number.
+
+    url= "https://en.wikipedia.org/w/api.php?action=query&prop=pageprops&ppprop=wikibase_item&redirects=1&titles=Arnolfini_portrait&format=json"
+    response = urllib.urlopen(url)
+
+    data = json.loads(response.read())
+    print(data)
