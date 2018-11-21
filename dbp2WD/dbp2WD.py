ffbaafa10ce83d49ba27a5fb3948248c32d20724
Terence Gould <terence@thegoulds.me>
2018-11-21 17:06:32 +0000

    Updated to return WikiID from dbp_id variable

--- dbp2WD/dbp2WD.py
+++ dbp2WD/dbp2WD.py
@@ -1,7 +1,10 @@
-import urllib, json
+import requests, json
+
+
 def main():
     print ("Hello")
-    getWD("Test")
+    getWD("Arnolfini_portrait")
+    getWD("No._5,_1948")
 
 def getWD(dbp_id):
     #Function to call the Wikipedia API
@@ -9,8 +12,17 @@ def getWD(dbp_id):
     #Will create response in JSON
     # which can be used to get the Q number.
 
-    url= "https://en.wikipedia.org/w/api.php?action=query&prop=pageprops&ppprop=wikibase_item&redirects=1&titles=Arnolfini_portrait&format=json"
-    response = urllib.urlopen(url)
+    url="https://en.wikipedia.org/w/api.php?action=query&prop=pageprops&ppprop=wikibase_item&redirects=1&titles=" + dbp_id + "&format=json"
+    r= requests.get(url)
+    data = r.json()
+    print(len(data))
+    #print(data['query']['pages']['1036801']['pageprops']['wikibase_item'])
+    #Need to iterate over the next bit to get pass the page ID - which we may not know
+    page = data['query']['pages']
+    for key in page:
+        WDID= page[key]['pageprops']['wikibase_item']
+        print(WDID)
 
-    data = json.loads(response.read())
-    print(data)
+
+if __name__ == "__main__":
+    main()
